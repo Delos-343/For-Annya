d5c28754fd44b447c2b2f9fcfab4547ab1caa7bb
Delos-343 <mfachry.handoko@gmail.com>
2023-02-13 14:18:07 +0700

    added HomePage to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { HomePage } from './routes'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element='' />
+        <Route exact path='/' element={<HomePage />} />
       </Routes>
     </>
   )

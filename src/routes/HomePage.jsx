9133c7e9806bc5de202a873912db9c5313e0ce49
Delos-343 <mfachry.handoko@gmail.com>
2023-02-13 14:12:55 +0700

    added HelloWorld component to HomePage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { HelloWorld } from '../components'
 
 const HomePage = () => {
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <HelloWorld />
     </div>
   )
 }
